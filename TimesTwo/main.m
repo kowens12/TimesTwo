d17b24c8d171061649d6f19abe6289470ae92f7b
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2017-04-27 14:34:49 -0400

    Initial Commit

---
+++ TimesTwo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by Katherine Owens on 4/27/17.
+//  Copyright © 2017 Katherine Owens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
